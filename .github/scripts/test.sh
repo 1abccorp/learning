b11657f3b10deb17bda26c20031027941f696ab8
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:44:03 -0500

    sd

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get the full path of the file that was changed
+file_path=$(git diff-tree --no-commit-id --name-only -r HEAD | head -n 1)
+
 # Get the name of the folder where the change was made
-folder_name=$(git diff-tree --no-commit-id --name-only -r HEAD | awk -F/ '{print $1}' | uniq)
+folder_name=$(dirname "$file_path")
 
 # Change to the root of the repo
 cd "$GITHUB_WORKSPACE"
