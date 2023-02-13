ba4bfe22ec2e439c27a33e342ad74b2675eaf24a
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:50:35 -0500

    d

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -5,7 +5,7 @@ GITHUB_WORKSPACE=$(pwd)
 
 # Get the full path of the file that was changed
 file_path=$(git diff-tree --no-commit-id --name-only -r HEAD | head -n 1)
-
+git diff-tree --no-commit-id --name-only HEAD
 # Get the name of the folder where the change was made
 folder_name=$(dirname "$file_path")
 
