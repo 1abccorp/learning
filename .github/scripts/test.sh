ddc61be2391dcc5a53a4e5841e1a6b53346ccf5a
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:26:31 -0500

    new commit
    tefd

---
+++ .github/scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the name of the folder where the change was made
+folder_name=$(git diff-tree --no-commit-id --name-only -r HEAD | awk -F/ '{print $1}' | uniq)
+
+# Change to the folder where the change was made
+cd "$folder_name"
