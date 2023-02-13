c90e15fba0654b6f9e2e82e498bf1cb31cf4d2d4
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-13 00:01:16 -0500

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# Folders to exclude
+exclude=(.git .github)
+
 # Get the list of folders in the root of the repository
 folders=$(find . -maxdepth 1 -type d | grep -v "^\.$")
 
 # Count the number of folders
-num_folders=$(echo "$folders" | wc -l)
+num_folders=0
 
 # Loop over each folder
 for folder in $folders; do
-  # Change into the folder
-  cd "$folder"
+  # Check if the folder is in the exclusion list
+  if [[ ! " ${exclude[@]} " =~ " ${folder} " ]]; then
+    # Change into the folder
+    cd "$folder"
 
-  # List the contents of the folder
-  echo "Contents of folder $folder:"
-  ls
+    # List the contents of the folder
+    echo "Contents of folder $folder:"
+    ls
 
-  # Change back to the root of the repository
-  cd ..
+    # Change back to the root of the repository
+    cd ..
+
+    # Increment the counter
+    ((num_folders++))
+  fi
 done
 
 # Print the number of folders
 echo "Number of folders: $num_folders"
+
