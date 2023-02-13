da9a3289c9f80a93927f702a4487f228395d9764
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:32:19 -0500

    efefe

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -3,5 +3,8 @@
 # Get the name of the folder where the change was made
 folder_name=$(git diff-tree --no-commit-id --name-only -r HEAD | awk -F/ '{print $1}' | uniq)
 
+echo "$folder_name"
 # Change to the folder where the change was made
 cd "$folder_name"
+
+echo "$pwd"
