7626f1cf66d162876ad7436a997bfabcff08a126
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:46:37 -0500

    hj

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# Set the GITHUB_WORKSPACE variable manually
+GITHUB_WORKSPACE=$(pwd)
+
 # Get the full path of the file that was changed
 file_path=$(git diff-tree --no-commit-id --name-only -r HEAD | head -n 1)
 
 # Get the name of the folder where the change was made
 folder_name=$(dirname "$file_path")
 
-# Change to the root of the repo
-cd "$GITHUB_WORKSPACE"
-
 # Change to the folder where the change was made
-cd "$folder_name"
+cd "$GITHUB_WORKSPACE/$folder_name"
+
 
 pwd
