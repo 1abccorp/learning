42c95591cb78b559d58546f16ff9f8ea0082fee5
OGHuman <51464705+OGHuman@users.noreply.github.com>
2023-02-12 23:58:05 -0500

    jihuh
    hu

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-x=$(git diff-tree --no-commit-id --name-only -r HEAD)
-echo "$x"
+# Get the list of folders in the root of the repository
+folders=$(find . -maxdepth 1 -type d | grep -v "^\.$")
 
-y=$(git diff-tree --no-commit-id --name-only -r HEAD | xargs dirname | sort | uniq)
-echo "$y"
+# Count the number of folders
+num_folders=$(echo "$folders" | wc -l)
 
+# Loop over each folder
+for folder in $folders; do
+  # Change into the folder
+  cd "$folder"
 
-pwd
+  # List the contents of the folder
+  echo "Contents of folder $folder:"
+  ls
+
+  # Change back to the root of the repository
+  cd ..
+done
+
+# Print the number of folders
+echo "Number of folders: $num_folders"
